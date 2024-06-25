8ba3c48926f58370a7e8eca5e34b1ff11cd71464
AbdulMoiz-Ali <abdulmoizali321@gmail.com>
2024-06-25 22:08:43 +0500

    number

---
+++ app.js
@@ -0,0 +1,10 @@
+var age = "I am 15 year old"
+var message = 'you have visited this site 14 time'
+alert(age)
+alert(message)
+var birthyear = 'My birth year is 1990\rDate type of my ddecleared Variables is numbers'
+var visitorname = 'Jhon Doe'
+var producttitle = 'T-shirt'
+var quantity = 5
+var email = 'XYZ Clouthing store'
+document.write(visitorname + ' ordered ' + producttitle + ' on ' + email)
